02c84d856776b6d9ae61fdf83dffb50a3ed70004
Norton <norton@alunos.utfpr.edu.br>
2017-09-19 15:14:21 -0300

    Set up Puma, Nginx & Capistrano 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,11 +63,11 @@ namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/production`
-        puts "WARNING: HEAD is not the same as origin/production"
-        puts "Run `git push` to sync changes."
-        exit
-      end
+    #  unless `git rev-parse HEAD` == `git rev-parse origin/production`
+     #   puts "WARNING: HEAD is not the same as origin/production"
+      #  puts "Run `git push` to sync changes."
+       # exit
+      #end
     end
   end
 
