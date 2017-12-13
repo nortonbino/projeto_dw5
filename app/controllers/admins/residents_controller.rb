090418f6061baed921737420d4a4ba44bb68946b
Norton <norton@alunos.utfpr.edu.br>
2017-12-13 12:20:52 -0200

    Solving conflicts and ManyToMany

--- app/controllers/admins/residents_controller.rb
+++ app/controllers/admins/residents_controller.rb
@@ -14,28 +14,36 @@ class Admins::ResidentsController < ApplicationController
 
     # Valida se o residente já existe na lista do condominio  
     if @condominium.residents.include?(@resident_by_email)
+      flash[:error] = "Resident is already in the condominium"
       render :new 
     else
       # Valida se o residente já existe no banco de dados 
       if @resident_by_email
         @condominium.residents << @resident_by_email
-        redirect_to admins_condominium_path(@condominium) 
+        flash[:success] = "Resident added at the condominium" 
+        redirect_to admins_condominium_residents_path(@condominium)
       end
     end
-  	
-    @resident = @condominium.residents.build
+    
+    @resident = @condominium.residents.build  
   end
 
   def create
   	@resident = @condominium.residents.build resident_params
-    @condominium.residents << @resident
-  	if @resident.save
-  		flash[:success] = "Resident created"
-  		redirect_to admins_condominium_path(@condominium)
-  	else
-  		flash.now[:error] = "Something went wrong"
-  		render :new
-  	end
+    if @resident.save
+      @condominium.residents << @resident
+      flash[:success] = "Resident created and added to the condominium"
+      redirect_to admins_condominium_residents_path(@condominium)
+    else
+      flash[:error] = "Make sure all the fields are correct"
+      redirect_to new_admins_condominium_resident_path
+    end
+  end
+
+  def destroy
+    # @resident.destroy
+    flash[:success] = 'resident was successfully destroyed.'
+    redirect_to admins_condominium_residents_path
   end
 
   def search_by_email(email)
